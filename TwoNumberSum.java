8e0cf347eed68e7dca3ba187325d5cd687755b5e
suhas kashyap <suhaskashyap2@gmail.com>
2021-01-04 21:19:33 +0530

    Add files via upload
    Arrays

---
+++ TwoNumberSum.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class TwoNumberSum {
+    public int[] twoNumberSumSolution1(int[] array, int targetSum) {
+        Arrays.sort(array);
+        int left=0;
+        int right=array.length-1;
+        while(left<right){
+            int currentSum=array[left]+array[right];
+            if(currentSum==targetSum){
+                return new int[] {array[left], array[right]};
+            }
+            else if(currentSum<targetSum){
+                left++;
+            }else {
+                right--;
+            }
+
+        }
+        return new int[0];
+    }
+
+    public int[] twoNumberSumSolution2(int [] array, int targetSum){
+        for(int index=0;index<array.length-1;index++){
+            int firstNum = array[index];
+            for (int nextIndex = index+1; nextIndex < array.length; nextIndex++) {
+                int secondNum=array[nextIndex];
+                int currentSum=firstNum+secondNum;
+                if(currentSum==targetSum){
+                    return new int[] {firstNum,secondNum};
+                }
+            }
+        }
+        return new int[0];
+
+    }
+
+    public static void main(String[] args) {
+       TwoNumberSum test= new TwoNumberSum();
+       int [] output=test.twoNumberSumSolution1(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10);
+        System.out.println(Arrays.toString(output));
+        int [] output1=test.twoNumberSumSolution2(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10);
+        System.out.println(Arrays.toString(output1));
+    }
+}
+
